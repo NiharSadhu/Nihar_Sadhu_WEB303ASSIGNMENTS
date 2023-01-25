3e2a14fb3aa70eb6077debfcda63e09124bee208
NiharSadhu <113557223+NiharSadhu@users.noreply.github.com>
2023-01-25 16:44:37 -0500

    comment

--- script.js
+++ script.js
@@ -5,25 +5,35 @@
 
 $(document).ready(function () {
 
+	// declaring the variables to store the values from the html DOM.
 	let $salary = $("#yearly-salary");
 	let $percent = $("#percent");
 	let $input = $("input");
 	let $span = $("#amount")
 
+	// Creating the event function 
 	$input.keyup(function () {
 
+		// variable to store the value for salary
 		let sal = $salary.val();
+		// converting the string to int
 		let salN = parseInt(sal);
+		// experiment purpose
 		console.log(salN);
 
+
+		// variable to store the value for percent
 		let per = $percent.val();
 		let perN = parseInt(per);
 		console.log(perN);
 
+		// calculation for getting the percentage
 		let arithmetic = salN * perN / 100;
+		// converting int to decimal
 		let aD = parseFloat(arithmetic);
 		console.log(arithmetic);
 
+		// inputting the value to the span element in html
 		$span.text("$" + aD.toFixed(2));
 
 	});
